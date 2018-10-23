e9b480faabdf19cdce606a23cf0e843629903760
Nasir <iamnasir368@gmail.com>
2018-10-24 02:31:03 +0600

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,14 @@
 $(document).ready(function () {
     "use strict"; // Start of use strict
 
-    $("#mobile-menu").metisMenu();
-
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal",
-        scrollInertia: 100
-    });
-
-    $('.overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
-        $('.overlay').fadeOut();
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').addClass('active');
-        $('.overlay').fadeIn();
-    });
-
-
-    $("#userSidebar").mCustomScrollbar({
-        theme: "minimal",
-        scrollInertia: 100
-    });
-
-    $('.overlay').on('click', function () {
-        $('#userSidebar').removeClass('active');
-        $('.overlay').fadeOut();
-    });
-
-    $('#userSidebarCollapse').on('click', function () {
-        $('#userSidebar').addClass('active');
-        $('.overlay').fadeIn();
-    });
-
-    //Searc bar
-
-    $('.dismiss, .overlay').on('click', function () {
-        $('.search-bar').removeClass('active');
-        $('.overlay').fadeOut();
-    });
-
-    $('#searchBarCollapse').on('click', function () {
-        $('.search-bar').addClass('active');
-        $('.overlay').fadeIn();
+    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
+        if ($(window).width() > 768) {
+            var e0 = e.originalEvent,
+                    delta = e0.wheelDelta || -e0.detail;
+            this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+            e.preventDefault();
+        }
     });
 
     //Select2
@@ -54,5 +19,46 @@ $(document).ready(function () {
     $('.datepicker').datepicker({
         autoclose: true
     });
+
+    //spinner
+    $('.spinner .btn:first-of-type').on('click', function () {
+        var btn = $(this);
+        var input = btn.closest('.spinner').find('input');
+        if (input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {
+            input.val(parseInt(input.val(), 10) + 1);
+        } else {
+            btn.next("disabled", true);
+        }
+    });
+    $('.spinner .btn:last-of-type').on('click', function () {
+        var btn = $(this);
+        var input = btn.closest('.spinner').find('input');
+        if (input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {
+            input.val(parseInt(input.val(), 10) - 1);
+        } else {
+            btn.prev("disabled", true);
+        }
+    });
+
+    //sortable
+    $(function () {
+        $("#sortable").sortable();
+        $("#sortable").disableSelection();
+    });
+
+    //slimScroll
+    $('.sortable--list').slimScroll({
+        height: '518px',
+        size: '5px',
+        distance: '0px',
+        color: '#aeafbc',
+        alwaysVisible: true,
+        railVisible: true,
+        railColor: '#e3e4ed',
+        railOpacity: 1
+    });
+
+    //metisMenu
+    $("#sortable").metisMenu();
 
 });
